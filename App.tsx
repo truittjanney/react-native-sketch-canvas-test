5b555cf8ad17421493566a4a4f788943cb2411cc
Truitt Janney <truittjanney@unknowna2542f82d648.attlocal.net>
2025-08-26 10:40:03 -0500

    build working with React Native 79.5

--- App.tsx
+++ App.tsx
@@ -18,16 +18,7 @@ function App(): React.JSX.Element
           canvasStyle={styles.canvas}
           defaultStrokeIndex={0}
           defaultStrokeWidth={5}
-          onStrokeStart={() => {
-            console.log('Touch started - drawing began!');
-          }}
-          onStrokeChanged={() => {
-            console.log('Stroke in progress...');
-          }}
-          onStrokeEnd={() => {
-            console.log('Touch ended - stroke complete!');
-          }}
-        />
+      />
       </View>
     
     </View>
@@ -69,42 +60,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
-// import { NewAppScreen } from '@react-native/new-app-screen';
-// import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-// import {
-//   SafeAreaProvider,
-//   useSafeAreaInsets,
-// } from 'react-native-safe-area-context';
-
-// function App() {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   return (
-//     <SafeAreaProvider>
-//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//       <AppContent />
-//     </SafeAreaProvider>
-//   );
-// }
-
-// function AppContent() {
-//   const safeAreaInsets = useSafeAreaInsets();
-
-//   return (
-//     <View style={styles.container}>
-//       <NewAppScreen
-//         templateFileName="App.tsx"
-//         safeAreaInsets={safeAreaInsets}
-//       />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
-
-// export default App;
